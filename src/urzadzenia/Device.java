4e021e5a22c37f9ecb8612a267003ffcf9b6e6fc
Wojciech T <hadwao2@gmail.com>
2018-09-16 18:01:11 +0200

    zad 3.2b

---
+++ src/urzadzenia/Device.java
@@ -0,0 +1,26 @@
+package urzadzenia;
+
+public class Device {
+    String code;
+    String producer;
+    String typ;
+    double price;
+
+    Device (String code, String producer, String typ, double price){
+        this(code, producer, typ);
+        this.price = price;
+    }
+
+    Device (String code, String producer, String typ){
+        this.code = code;
+        this.producer = producer;
+        this.typ = typ;
+    }
+
+    String getInfo (){
+        return (" KOD: " + this.code + ", producent: " + this.producer + ", typ: " + this.typ + ", cena: " + this.price);
+    }
+
+
+
+}
